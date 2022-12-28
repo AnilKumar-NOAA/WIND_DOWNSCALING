43340c0f1e3ba5d5805eb279296cd9859b571df4
Anil Kumar <108816337+AnilKumar-NOAA@users.noreply.github.com>
2022-12-28 10:59:13 -0500

    Add files via upload
    new changes

---
+++ R_IDA_RPLOT/petit.R
@@ -0,0 +1,96 @@
+library(ggplot2)
+library(zoo)
+getwd()
+
+setwd("/Users/anilkumar/IDA_RPLOT")
+
+wind.df3 = read.csv(file="petit_hourly.csv")
+attach(wind.df3)
+head(wind.df3)
+tail(wind.df3)
+
+par(mfrow=c(2,2))
+OBS_WSPD[OBS_WSPD == 9999] <- NA
+OBS_PSFC[OBS_PSFC == 9999] <- NA
+OBS_WDIR[OBS_WDIR == 9999] <- NA
+OBS_GUST[OBS_GUST == 9999] <- NA
+OBS_GUST[OBS_GUST == 5139.49] <- NA
+OBS_WSPD[OBS_WSPD == 5139.49] <- NA
+plot(OBS_WSPD,  type='o', col="black", ylim=c(0,40), xaxt="n",ann=FALSE, cex.axis=1.5, cex=1.5)
+
+lines(HWRFLES_WSPD, type="o", pch=23, lty=2, col="red")
+
+
+# Create a title with a red, bold/italic font
+title(main="Petit Bois, MS \n Station - 8741003", col.main="black", cex=4)
+#title("Eagle Point , TX \n Station 8771013", cex.main = 1.5, col.main= "black")
+
+# Label the x and y axes with dark green text
+title(xlab="(UTC/Date) Aug 2021", col.lab=rgb(0,0,0), cex.lab=1.5)
+title(ylab="Wind Speed (mph)", col.lab=rgb(0,0,0), cex.lab=1.5)
+
+# Create a legend at (1, g_range[2]) that is slightly smaller 
+# (cex) and uses the same line colors and points used by 
+# the actual plots 
+legend(1, 30, c("OBS","HWRF-LES"), cex=0.90, 
+       col=c("black","red"), pch=21:22, lty=1:2);
+labels1 = c("18/28","00/29","06/29","12/29","18/29","00/30","06/30","12/30")
+
+axis(1, at=seq(1,48,6), labels=labels1,lwd=2, cex.axis=1.5, cex=1.5)
+
+# PLOT WIND GUST
+
+plot(OBS_GUST,  type='o', col="black", ylim=c(0,40), xaxt="n",ann=FALSE, cex.axis=1.5, cex=1.5)
+
+lines(HWRFLES_GUST, type="o", pch=23, lty=2, col="red")
+
+# Create a title with a red, bold/italic font
+title(main="Petit Bois, MS \n Station - 8741003", col.main="black", cex=4)
+#title("Eagle Point , TX \n Station 8771013", cex.main = 1.5, col.main= "black")
+
+# Label the x and y axes with dark green text
+title(xlab="(UTC/Date) Aug 2021", col.lab=rgb(0,0,0), cex.lab=1.5)
+title(ylab="Wind Gust (mph)", col.lab=rgb(0,0,0), cex.lab=1.5)
+
+# Create a legend at (1, g_range[2]) that is slightly smaller 
+# (cex) and uses the same line colors and points used by 
+# the actual plots 
+legend(1, 30, c("OBS","HWRF-LES"), cex=0.90, 
+       col=c("black","red"), pch=21:22, lty=1:2);
+abels1 = c("18/28","00/29","06/29","12/29","18/29","00/30","06/30","12/30")
+
+axis(1, at=seq(1,48,6), labels=labels1,lwd=2, cex.axis=1.5, cex=1.5)
+
+
+plot(OBS_PSFC,  type='o', col="black", ylim=c(980,1020), xaxt="n",ann=FALSE, cex.axis=1.5, cex=1.5)
+
+
+lines(HWRFLES_PSFC, type="o", pch=23, lty=2, col="red")
+
+title(main="Petit Bois, MS \n Station - 8741003", col.main="black", cex=4)
+title(xlab="(UTC/Date) Aug 2021", col.lab=rgb(0,0,0), cex.lab=1.5)
+title(ylab="Sfc Press (mb)", col.lab=rgb(0,0,0), cex.lab=1.5)
+legend(1, 990, c("OBS","HWRF-LES"), cex=0.90, 
+       col=c("black","red"), pch=21:22, lty=1:2);
+abels1 = c("18/28","00/29","06/29","12/29","18/29","00/30","06/30","12/30")
+
+axis(1, at=seq(1,48,6), labels=labels1,lwd=2, cex.axis=1.5, cex=1.5)
+
+plot(OBS_WDIR,  type='o', col="black", ylim=c(0,360), xaxt="n",ann=FALSE, cex.axis=1.5, cex=1.5)
+box()
+
+lines(HWRFLES_WDIR, type="o", pch=22, lty=2, col="red")
+
+title(main="Petit Bois, MS \n Station - 8741003", col.main="black", cex=4)
+title(xlab="(UTC/Date) Aug 2021", col.lab=rgb(0,0,0), cex.lab=1.5)
+
+title(ylab="Wind Dir (deg)", col.lab=rgb(0,0,0), cex.lab=1.5)
+
+# Create a legend at (1, g_range[2]) that is slightly smaller 
+# (cex) and uses the same line colors and points used by 
+# the actual plots 
+legend(400, 100, c("OBS","HWRF-LES-HighRES"), cex=0.40, 
+       col=c("black","red"), pch=21:22, lty=1:2);
+abels1 = c("18/28","00/29","06/29","12/29","18/29","00/30","06/30","12/30")
+
+axis(1, at=seq(1,48,6), labels=labels1,lwd=2, cex.axis=1.5, cex=1.5)
